addd0e87c1f77036eca62b139c6341840cc8faef
GedsonCS <gedsoncandido94@gmail.com>
2023-07-28 00:48:55 -0300

    feat/cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import "express-async-errors";
 import express, { Application, application } from "express";
 import { userRoutes } from "./routes/users.routes";
-import { handleAppError } from "./middlewares/handleAppError.middleware";
 import { handleErros } from "./error";
 import { loginUserRoutes } from "./routes/login.routes";
 import { contsctRoutes } from "./routes/contacts.routes";
+import cors from "cors";
+
 const app: Application = express();
 app.use(express.json());
 
+app.use(cors);
 app.use("/users", userRoutes);
 app.use("/login", loginUserRoutes);
 app.use("/contact", contsctRoutes);
-//app.use(handleAppError);
 app.use(handleErros);
 export default app;
